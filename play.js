b915a431e88d55670326cb2ea2286b92a0f97ba3
jabrilg <jabrilgadhane@hotmail.ca>
2021-12-06 22:14:29 +0000

    first commit

---
+++ play.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+process.stdout.write('\x07');
+
+const { Game } = require('./src/Game')
+const { UserInterface } = require('./src/UserInterface')
+const { RemoteInterface } = require('./src/RemoteInterface')
+const game = new Game(new UserInterface(), new RemoteInterface())
+
+/
+game.start()
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '192.168.2.14',// IP address here,
+    port: "50541",// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
